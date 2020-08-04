9a31aab81fd8fca52794ce199b1778b88dc0843a
Abhishek4298 <abhishek.bhavsar@bacancytechnology.com>
2020-08-04 16:42:45 +0530

    structure formation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const { getFormattedTime } = require("./helper/index");
 
 (async () => {
 	const movieUrl = 'https://www.imdb.com/title/tt5753856/?ref_=nv_sr_srsg_0';
@@ -9,16 +10,6 @@ const puppeteer = require('puppeteer');
 	await page.goto(movieUrl, { waitUntil: 'networkidle2' });
 	await page.screenshot({ path: 'screenshot-' + getFormattedTime() + '.png' });
 
-	function getFormattedTime() {
-		var today = new Date();
-		var y = today.getFullYear();
-		var m = today.getMonth() + 1;
-		var d = today.getDate();
-		var h = today.getHours();
-		var mi = today.getMinutes();
-		var s = today.getSeconds();
-		return y + "-" + m + "-" + d + "-" + h + "-" + mi + "-" + s;
-	}
 	let data = await page.evaluate(() => {
 
 		let title = document.querySelector('div[class="title_wrapper"] > h1').innerText;
