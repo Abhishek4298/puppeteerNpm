d2377d732382ca7cc9b90929d5f3a934500bbf4a
Abhishek4298 <abhishek.bhavsar@bacancytechnology.com>
2020-08-05 11:59:04 +0530

    exploring more on puppeteer

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const { getFormattedTime } = require("./helper/index");
 (async () => {
 	const movieUrl = 'https://www.imdb.com/title/tt5753856/?ref_=nv_sr_srsg_0';
 
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		headless:true,
+		devtools: true
+	});
 	const page = await browser.newPage();
 
 	await page.goto(movieUrl, { waitUntil: 'networkidle2' });
+
+	// for taking screenshot
 	await page.screenshot({ path: 'screenshot-' + getFormattedTime() + '.png' });
+
+	// for creating pdf file
+	await page.pdf({ path: 'new.pdf', format: 'A4' });
+
+	//To know the URL link.
+	page.on('console', msg => console.log('PAGE LOG :=>', msg.text()));
+	await page.evaluate(() => console.log(`The url is :::  ${location.href}`));
+
+	//to know viewport of the page
+	const dimensions = await page.evaluate(() => {
+		return {
+			width: document.documentElement.clientWidth,
+			height: document.documentElement.clientHeight,
+			deviceScaleFactor: window.devicePixelRatio
+		};
+	});
+	console.log('Dimensions :::', dimensions);
 
 	let data = await page.evaluate(() => {
 
